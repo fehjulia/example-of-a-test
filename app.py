d844ce1ea6b27bee8799be837f0607f5d8744b17
fernanda Julia <fernandajulia08@hotmail.com>
2024-08-01 23:10:45 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from time import sleep
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get('https://www.google.com/')
+sleep(60)
+
+driver.find_element('xpath','//*[@id="APjFqb"]').send_keys('teste')
+driver.find_element('xpath','/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]').click()
