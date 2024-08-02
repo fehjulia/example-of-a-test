2a043f9a27c498ad9459adae65874a232a63ffd1
fernanda Julia <fernandajulia08@hotmail.com>
2024-08-02 10:34:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from time import sleep
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get('https://www.google.com/')
-sleep(60)
+sleep(10)
 
 driver.find_element('xpath','//*[@id="APjFqb"]').send_keys('teste')
 driver.find_element('xpath','/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]').click()
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+input()
